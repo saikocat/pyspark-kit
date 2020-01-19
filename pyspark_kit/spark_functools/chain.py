2dc88402fbde4480e11c260778fe332eb7a9d172
Hoa <hoameomu@gmail.com>
2020-01-19 22:49:11 +0800

    [functools] add chain_transforms functional helper

---
+++ pyspark_kit/spark_functools/chain.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+from typing import Any, Callable, List
+
+from cytoolz.functoolz import compose
+
+from pyspark.sql import DataFrame as SparkDataFrame
+
+
+def chain_transforms(
+    curried_funcs: List[Callable[[Any, SparkDataFrame], SparkDataFrame]],
+    df: SparkDataFrame,
+) -> SparkDataFrame:
+    """Chain multiple transformations to the source dataframe
+
+    Python API for DataFrame doesn't support .transforms() so this helper makes composing multiple transformations
+    easier.
+
+    Args:
+        curried_funcs (List[Callable[[Any, SparkDataFrame], SparkDataFrame]]): List of curried functions (annotated
+            with cytoolz.curry) each is a transformation for source DataFrame
+        df (SparkDataFrame)
+
+    Returns:
+        dataframe after being applied all transformations
+
+    Examples:
+        >>> @curry
+        >>> def with_label(label, df):
+        >>>     return df.withColumn("label", lit(label))
+        >>> ...
+        >>> actual_df = chain_transforms([with_label("offline")], source_df)
+    """
+    chained = compose(*reversed(curried_funcs))
+    return chained(df)
