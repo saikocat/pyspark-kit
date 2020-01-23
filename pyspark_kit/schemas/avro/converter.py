cad3061236e81e707d662296d2dcba3d3e88cbf5
Hoa <hoameomu@gmail.com>
2020-01-23 22:35:50 +0800

    [schema] add to_spark_schema datatype for avro converter

--- pyspark_kit/schemas/avro/converter.py
+++ pyspark_kit/schemas/avro/converter.py
@@ -1,7 +1,8 @@
 from typing import Union
 
+from py4j.java_gateway import java_import
 from pyspark.sql import SparkSession
-from pyspark.sql.types import StructType
+from pyspark.sql.types import DataType, StructType, _parse_datatype_json_string
 
 
 class AvroSchemaConverter:
@@ -12,14 +13,18 @@ class AvroSchemaConverter:
 
     def __init__(self, spark: SparkSession):
         """TODO: find a way to pass existing SparkConf instead"""
-        from py4j.java_gateway import java_import
-
         java_import(spark._jvm, "org.apache.spark.sql.avro.SchemaConverters")
+        java_import(spark._jvm, "org.apache.avro.Schema")
         self.spark = spark
         self.structype_from_json = (
             spark._jvm.org.apache.spark.sql.types.StructType.fromJson
         )
         self.schema_converters = spark._jvm.org.apache.spark.sql.avro.SchemaConverters
+
+    def to_spark_schema(self, avsc_json: str) -> DataType:
+        avsc_jvm = self.spark._jvm.org.apache.avro.Schema.Parser().parse(avsc_json)
+        spark_type_jvm = self.schema_converters.toSqlType(avsc_jvm)
+        return _parse_datatype_json_string(spark_type_jvm.dataType().json())
 
     def to_json_from_spark_schema(
         self,
